dba8e2b7c47dd554646667c6554d7aaac5ec8eab
Térence <terence.meunier@gmail.com>
2021-05-26 15:33:18 +0200

    feat(records): Add get route for create and update record #7

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,9 +6,33 @@ public class App {
         before("/*", (req, res) -> {
             res.header("Access-Control-Allow-Origin", "*");
         });
-        get("/", (req, res) -> menu.readAllRecords());
-        get("/:recordId", (req, res) -> {
+        get("/records", (req, res) -> menu.readAllRecords());
+        get("/records/:recordId", (req, res) -> {
             return menu.findRecord(req.params("recordId"));
+        });
+        get("/create", (req, res) -> {
+            menu.createRecord("Titanic", "Mr Ismet", "White Star Line", "Une belle histoire d'amour !");
+            res.redirect("/records");
+            return null;
+        });
+        get("/update/:recordId", (req, res) -> {
+
+            String title, author, editor, pitch;
+
+            if (req.queryParams("title") == null
+                    && req.queryParams("author") == null
+                    && req.queryParams("editor") == null
+                    && req.queryParams("pitch") == null) {
+                title = author = editor = pitch = "";
+            } else {
+                title = req.queryParams("title");
+                author = req.queryParams("author");
+                editor = req.queryParams("editor");
+                pitch = req.queryParams("pitch");
+            }
+            menu.updateRecord(req.params("recordId"), title, author, editor, pitch);
+            res.redirect("/records");
+            return null;
         });
     }
 }
